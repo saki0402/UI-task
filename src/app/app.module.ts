093f6867f6d4a7609714c7e5fced928df1c725cd
SaikiranReddy Anapala <anapalasaikiranreddy@gmail.com>
2021-05-30 19:33:22 +0530

    Initial commit: for the task of lenskart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule } from './footer/footer.module';
+import { HeaderModule } from './header/header.module';
+import { ProductListModule } from './product-list/product-list.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    FooterModule,
+    ProductListModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
